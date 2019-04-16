a11aa9d1dab35445b55dd83fbb81252e37d67154
Mguy11 <0911554@hr.nl>
2019-04-16 11:49:31 +0200

    EUhm..... Github crash, HALP!!!!  Nieuwe upload app

---
+++ GameChecker2/app/src/main/java/com/example/gamechecker/AddGame.java
@@ -0,0 +1,14 @@
+package com.example.gamechecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_game);
+    }
+}
